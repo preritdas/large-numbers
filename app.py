566c83ed89e936c2b7817e8cf30e3664decc4416
Prerit Das <preritdas@gmail.com>
2022-03-15 17:16:01 -0700

    Made it into law of large numbers.

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import random
 
-st.write("Hello world.")
+st.title("Law of Large Numbers")
 
-if __name__ == "__main__":
-    print("Hello world.")
+# How many random trials
+iterations = st.slider(label = "Number of iterations.", min_value = 5, max_value = 10000, value = 50)
+
+x = [_ for _ in range(iterations)] # create an array of consecutive integers
+y = [random.randint(0, 1) for _ in range(iterations)] # pick a random number for every iteration
+
+st.subheader("Your Random Numbers")
+
+st.line_chart(y)
+
+st.subheader("Moving Average")
+
+# Calculate average
+avg = []
+total = 0
+for pos, item in enumerate(y):
+    total += item # maintain a running total
+    avg.append(total/(pos+1)) # append avg with moving average
+
+st.line_chart(avg)
